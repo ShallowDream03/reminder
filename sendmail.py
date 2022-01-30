d7a88acdd7a244729a076a759667e628336613f5
ShallowDream03 <63938082+ShallowDream03@users.noreply.github.com>
2022-01-30 20:05:36 +0800

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -2,17 +2,16 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+
 def sendmail():
     # 设置服务器所需信息
     # qq邮箱服务器地址
     mail_host = 'smtp.qq.com'
-    # qq用户名
-    mail_user = 'xxxx@qq.com'
-    # 密码(部分邮箱为授权码)
+    # 密码(qq邮箱为授权码)
     mail_pass = 'xxxx'
-    # 邮件发送方邮箱地址
+    # 邮件发送方邮箱地址（你的QQ邮箱）
     sender = 'xxxx@qq.com'
-    # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发,可以是任意邮箱地址，不限于QQ
+    # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发，可以是任意邮箱，不限于QQ
     receivers = ['xxxx@qq.com']
 
     # 设置email信息
@@ -26,17 +25,11 @@ def sendmail():
     # 接受方信息
     message['To'] = receivers[0]
 
-    # file_path=r'C:\Users\pinoz\Desktop\2.png'
-    # att = MIMEText(open(file_path,"rb").read(), "base64", "utf-8")
-    # att["Content-Type"] = 'application/octet-stream'
-    # att["Content-Disposition"] = 'attachment; filename="2.png"'
-    # message.attach(att)
-
     # 登录并发送邮件
     try:
         smtpObj = smtplib.SMTP_SSL(mail_host, 465)
         # 登录到服务器
-        smtpObj.login(mail_user, mail_pass)
+        smtpObj.login(sender, mail_pass)
         # 发送
         smtpObj.sendmail(
             sender, receivers, message.as_string())
